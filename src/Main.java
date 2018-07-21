0270fd8ff8ec0e7599a984cfca20252a7df4859a
Simi <nioasca@gmail.com>
2018-07-21 17:12:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] wordListOne = {"27/7","multi-Tier","30000 foot","B-to-B","win-win"};
+       System.out.print(wordListOne[0]);
+    }
+}
